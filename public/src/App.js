2e1a511fd8faeb3070416026e8647f76548d66a7
xenon-glitch <58307661+xenon-glitch@users.noreply.github.com>
2025-03-01 16:14:38 +0545

    Update App.js

--- public/src/App.js
+++ public/src/App.js
@@ -1,1 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Column from "./components/Column";
+import Stirrups from "./components/Stirrups";
+import Slab from "./components/Slab";
+import Footing from "./components/Footing";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Column />} />
+          <Route path="/stirrups" element={<Stirrups />} />
+          <Route path="/slab" element={<Slab />} />
+          <Route path="/footing" element={<Footing />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
